5c7c67da9f623c7fd8212107b3746fc1de58c302
AlexanderAkh <sancheek240@mail.ru>
2023-01-20 16:06:19 +0300

    Add task1

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+System.Console.WriteLine("Введите ваше число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+ if (num > 99 && num < 1000)
+ {
+    System.Console.WriteLine($"2-я цифра {num} = {num / 10 % 10}");
+ }
+ else
+ {System.Console.WriteLine("Введите другое число");
+ }
