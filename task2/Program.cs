43f6e8a9567f45c5e0f894abfba570d3b0d8d12b
AlexanderAkh <sancheek240@mail.ru>
2023-01-23 09:35:11 +0300

    add task 2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+System.Console.WriteLine("Введите ваше число: ");
+string num = Console.ReadLine();
+ int length = num.Length;
+ if (length > 2)
+ {
+   System.Console.WriteLine($"{num[2]}");
+   
+ }
+ else
+{ 
+    System.Console.WriteLine("В числе меньше 3-х цифр ");
+}
